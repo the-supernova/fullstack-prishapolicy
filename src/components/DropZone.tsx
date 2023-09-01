9d3fe9d1fbce61d125d7d56a097304cea49d181d
the-supernova <biswalabhijit04@gmail.com>
2023-09-02 00:47:40 +0530

    feat:drag and drop for files

--- src/components/DropZone.tsx
+++ src/components/DropZone.tsx
@@ -1,69 +1,99 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, DragEvent, SetStateAction, useState } from "react";
 
 type Props = {
-    selectedFile: string;
-    setSelectedFile: Dispatch<SetStateAction<string>>;
-}
+  selectedFile: string;
+  setSelectedFile: Dispatch<SetStateAction<string>>;
+};
 
 export default function DropZone({ selectedFile, setSelectedFile }: Props) {
-    return (
-        <div className="flex">
-                <label
-                  htmlFor="dropzone-file"
-                  className="flex flex-col items-center justify-center h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 p-12"
-                >
-                  <div className="flex flex-col items-center justify-center ">
-                    <svg
-                      className="w-8 h-8 mb-4 text-[#27378C]"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 20 16"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                      />
-                    </svg>
-                    {selectedFile === "" ? (
-                      <>
-                        <p className="mb-2 text-sm text-gray-500">
-                          <span className="font-semibold text-[#27378C] underline underline-offset-4">
-                            Browse
-                          </span>{" "}
-                          or drop file here
-                        </p>
-                        <p className="text-xs text-gray-500">
-                          Supports: PDF; upto 10MB
-                        </p>
-                      </>
-                    ) : (
-                      <p className="mb-2 text-sm text-gray-500">
-                        {selectedFile}
-                      </p>
-                    )}
-                  </div>
-                  <input
-                    id="dropzone-file"
-                    name="dropzone-file"
-                    type="file"
-                    accept="application/pdf"
-                    onChange={(e) => {
-                      if (e.target.files) {
-                        if (e.target.files.length) {
-                            setSelectedFile(e.target.files[0].name);
-                        } else {
-                            setSelectedFile("");
-                        }
-                      }
-                    }
-                    }
-                    className="hidden"
-                  />
-                </label>
-              </div>
-    )
+  const [dragActive, setDragActive] = useState(false);
+
+  const handleDrag = (e: DragEvent<HTMLLabelElement> | DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.type === 'dragenter' || e.type === 'dragover') {
+      setDragActive(true);
+    } else if (e.type === 'dragleave') {
+      setDragActive(false);
+    }
+  }
+
+  const handleDrop = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer.files) {
+      if (e.dataTransfer.files.length) {
+        if (e.dataTransfer.files[0].size > 10 * 1024 * 1024) {
+          alert("File size should be less than 10MB");
+          return;
+        }
+        setSelectedFile(e.dataTransfer.files[0].name);
+      } else {
+        setSelectedFile("");
+      }
+    }
+  }
+  return (
+    <div className="flex">
+      <label
+        htmlFor="dropzone-file"
+        className="flex flex-col items-center justify-center h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 p-12"
+        onDragEnter={handleDrag}
+      >
+        <div className="flex flex-col items-center justify-center ">
+          <svg
+            className="w-8 h-8 mb-4 text-[#27378C]"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 16"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+            />
+          </svg>
+          {selectedFile === "" ? (
+            <>
+              <p className="mb-2 text-sm text-gray-500">
+                <span className="font-semibold text-[#27378C] underline underline-offset-4">
+                  Browse
+                </span>{" "}
+                or drop file here
+              </p>
+              <p className="text-xs text-gray-500">Supports: PDF; upto 10MB</p>
+            </>
+          ) : (
+            <p className="mb-2 text-sm text-gray-500">{selectedFile}</p>
+          )}
+        </div>
+        <input
+          id="dropzone-file"
+          name="dropzone-file"
+          required
+          type="file"
+          accept="application/pdf"
+          onChange={(e) => {
+            if (e.target.files) {
+              if (e.target.files.length) {
+                if (e.target.files[0].size > 10 * 1024 * 1024) {
+                  alert("File size should be less than 10MB");
+                  return;
+                }
+                setSelectedFile(e.target.files[0].name);
+              } else {
+                setSelectedFile("");
+              }
+            }
+          }}
+          className="hidden"
+        />
+        {dragActive && <div className="absolute w-full h-full inset-0" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div>}
+      </label>
+    </div>
+  );
 }
--- src/pages/add.tsx
+++ src/pages/add.tsx
@@ -4,16 +4,40 @@ import { trpc } from "@/utils/trpc";
 import Image from "next/image";
 import axios from "axios";
 import { useRouter } from "next/router";
-import { FormEvent, useState } from "react";
+import { DragEvent, FormEvent, useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import { BiChevronLeft } from "react-icons/bi";
 
 export default function Add() {
   const router = useRouter();
+  const [dragActive, setDragActive] = useState(false);
   const [selectedFileName, setSelectedFileName] = useState("");
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const addBookMutation = trpc.postBookData.useMutation();
   const updateBookMutation = trpc.updateWithFiles.useMutation();
+
+  const handleDrag = (e: DragEvent<HTMLLabelElement> | DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.type === 'dragenter' || e.type === 'dragover') {
+      setDragActive(true);
+    } else if (e.type === 'dragleave') {
+      setDragActive(false);
+    }
+  }
+
+  const handleDrop = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer.files) {
+      if (e.dataTransfer.files.length) {
+        setSelectedFile(e.dataTransfer.files[0]);
+      } else {
+        setSelectedFile(null);
+      }
+    }
+  }
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -24,46 +48,55 @@ export default function Add() {
       author: formData.get("author") as string,
       readtime: Number(formData.get("readtime")),
       description: formData.get("details") as string,
-    }
+    };
 
     const imageData = new FormData();
-    imageData.append('file', formData.get('bookcover') as File)
-    imageData.append('upload_preset', 'bookData')
+    imageData.append("file", formData.get("bookcover") as File);
+    imageData.append("upload_preset", "bookData");
 
     const fileData = new FormData();
-    fileData.append('file', formData.get('dropzone-file') as File)
-    fileData.append('upload_preset', 'bookData')
+    fileData.append("file", formData.get("dropzone-file") as File);
+    fileData.append("upload_preset", "bookData");
 
     addBookMutation.mutate(bookData, {
       onSuccess: async (data) => {
         let imageURL, fileURL;
-        let uploadFileApiResponse = await axios.post('https://api.cloudinary.com/v1_1/dxkgx5g5i/auto/upload', fileData)
+        let uploadFileApiResponse = await axios.post(
+          "https://api.cloudinary.com/v1_1/dxkgx5g5i/auto/upload",
+          fileData
+        );
 
         if (!uploadFileApiResponse.data) {
-          console.log('Error uploading filedata', uploadFileApiResponse)
+          console.log("Error uploading filedata", uploadFileApiResponse);
           return;
         }
-        fileURL = uploadFileApiResponse.data.secure_url
+        fileURL = uploadFileApiResponse.data.secure_url;
 
-        let uploadImageApiResponse = await axios.post('https://api.cloudinary.com/v1_1/dxkgx5g5i/image/upload', imageData)
+        let uploadImageApiResponse = await axios.post(
+          "https://api.cloudinary.com/v1_1/dxkgx5g5i/image/upload",
+          imageData
+        );
         if (!uploadImageApiResponse.data) {
-          console.log('Error uploading imagedata', uploadImageApiResponse)
+          console.log("Error uploading imagedata", uploadImageApiResponse);
           return;
         }
-        imageURL = uploadImageApiResponse.data.secure_url
-        updateBookMutation.mutate({ id: data.id, cover: imageURL, pdf: fileURL }, {
-          onSuccess: () => {
-            router.push('/')
-          },
-          onError: (error) => {
-            console.log('Error uploading filedata', error)
+        imageURL = uploadImageApiResponse.data.secure_url;
+        updateBookMutation.mutate(
+          { id: data.id, cover: imageURL, pdf: fileURL },
+          {
+            onSuccess: () => {
+              router.push("/");
+            },
+            onError: (error) => {
+              console.log("Error uploading filedata", error);
+            },
           }
-        })
+        );
       },
       onError: (error) => {
-        console.log('error', error)
-      }
-    })
+        console.log("error", error);
+      },
+    });
 
     e.currentTarget.reset();
   };
@@ -72,7 +105,10 @@ export default function Add() {
       <NavBar />
       <section className="container mx-auto flex flex-col gap-6 px-10 py-5">
         <div>
-          <button className="flex items-center gap-2 border-2 border-[#27378C] rounded-lg px-6 py-2" onClick={() => router.push('/')}>
+          <button
+            className="flex items-center gap-2 border-2 border-[#27378C] rounded-lg px-6 py-2"
+            onClick={() => router.push("/")}
+          >
             <BiChevronLeft />
             <p className="text-[#27378C] hover:underline hover:underline-offset-4">
               Back to Home
@@ -85,6 +121,7 @@ export default function Add() {
             <label
               htmlFor="bookcover"
               className="w-full flex flex-col gap-4 items-center justify-center border-dashed border-2 border-[#27378C] rounded-lg hover:cursor-pointer relative overflow-hidden"
+              onDragEnter={handleDrag}
             >
               {!selectedFile ? (
                 <>
@@ -106,22 +143,20 @@ export default function Add() {
               type="file"
               id="bookcover"
               name="bookcover"
+              accept="image/*"
               className="hidden"
+              required
               onChange={({ target: { files } }) => {
                 if (files) {
-                    if (files.length) {
-                        const reader = new FileReader();
-                        reader.readAsDataURL(files[0]);
-                        reader.onload = (e) => {
-                        console.log("e.target.result", e.target?.result);
-                        };
-                        setSelectedFile(files[0]);
-                    } else {
-                        setSelectedFile(null);
-                    }
+                  if (files.length) {
+                    setSelectedFile(files[0]);
+                  } else {
+                    setSelectedFile(null);
+                  }
                 }
               }}
             />
+            {dragActive && <div className="absolute w-full h-full inset-0" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div>}
           </div>
 
           <div className="w-[50%] flex flex-col gap-4">
@@ -131,6 +166,7 @@ export default function Add() {
                 type="text"
                 id="title"
                 name="title"
+                required
                 placeholder="Enter the published name"
                 className="border-2 focus:outline-[#27378C] rounded-md p-2"
               />
@@ -142,6 +178,7 @@ export default function Add() {
                   type="text"
                   id="author"
                   name="author"
+                  required
                   placeholder="Add all the authors comma separated"
                   className="border-2 focus:outline-[#27378C] rounded-md p-2"
                 />
@@ -152,6 +189,7 @@ export default function Add() {
                   type="number"
                   id="readtime"
                   name="readtime"
+                  required
                   placeholder="Add time in mins"
                   className="border-2 focus:outline-[#27378C] rounded-md p-2"
                 />
@@ -162,6 +200,7 @@ export default function Add() {
               <textarea
                 id="details"
                 name="details"
+                required
                 rows={5}
                 placeholder="Should not be more than 300 words"
                 className="border-2 focus:outline-[#27378C] rounded-md p-2"
